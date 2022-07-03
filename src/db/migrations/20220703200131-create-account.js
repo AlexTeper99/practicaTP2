d0cdeab18ac646be94f6e8a8bc53899935fa844e
AlexTeper99 <ateper@fi.uba.ar>
2022-07-03 17:59:50 -0300

    account migration

---
+++ src/db/migrations/20220703200131-create-account.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Accounts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      address:{
+        allowNull: false,
+        unique: true,
+        type: Sequelize.INTEGER(16),
+      },
+      balance:{
+        type: Sequelize.INTEGER
+      },
+      clientId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Accounts');
+  }
+};
