ccd264a680ec09c83b83cd8912223f90cbbc405d
AlexTeper99 <ateper@fi.uba.ar>
2022-07-03 18:00:10 -0300

    account model

---
+++ src/db/models/account.js
@@ -0,0 +1,38 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Account extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Account.init({
+    id: {
+      allowNull: false,
+      type:DataTypes.INTEGER(16),
+      primaryKey: true,
+      autoIncrement: true
+    },
+    address: {
+      allowNull: false,
+      type:DataTypes.INTEGER(16),
+      unique: true
+    },
+    balance: DataTypes.INTEGER,
+    clientId:{ type: DataTypes.INTEGER, allowNull: false},
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+  }, {
+    sequelize,
+    modelName: 'Account',
+    tableName: 'accounts'
+  });
+  return Account;
+};
