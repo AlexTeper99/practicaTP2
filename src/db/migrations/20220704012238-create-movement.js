ec42c746e69a825cbc66215ac3d2252fbe938ef9
AlexTeper99 <ateper@fi.uba.ar>
2022-07-03 22:32:19 -0300

    migration movement

---
+++ src/db/migrations/20220704012238-create-movement.js
@@ -0,0 +1,29 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Movements', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      description: {
+        type: Sequelize.STRING(100)
+      },
+      ammount:{type: Sequelize.INTEGER},
+      accountId:{type: Sequelize.INTEGER},
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Movements');
+  }
+};
